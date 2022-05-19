c7686683a0154623e4582cee771bcdce70049b16
김세미 <semikimm.m@gmail.com>
2022-05-19 18:23:22 +0900

    [jdbc-test] PostUpdateController admin 도 수정 가능하도록 변경

--- board-mybatis/src/main/java/com/nhnacademy/jdbc/board/index/web/PostUpdateController.java
+++ board-mybatis/src/main/java/com/nhnacademy/jdbc/board/index/web/PostUpdateController.java
@@ -2,6 +2,10 @@ package com.nhnacademy.jdbc.board.index.web;
 
 import com.nhnacademy.jdbc.board.post.domain.Post;
 import com.nhnacademy.jdbc.board.post.service.PostService;
+import com.nhnacademy.jdbc.board.user.domain.User;
+import com.nhnacademy.jdbc.board.user.service.UserService;
+import java.sql.Timestamp;
+import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -10,15 +14,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.sql.Timestamp;
-import java.util.Date;
-
 @Controller
 public class PostUpdateController {
     private final PostService postService;
+    private final UserService userService;
 
-    public PostUpdateController(PostService postService) {
+    public PostUpdateController(PostService postService,
+                                UserService userService) {
         this.postService = postService;
+        this.userService = userService;
     }
 
     @GetMapping("/post/update")
@@ -26,13 +30,16 @@ public class PostUpdateController {
                                        HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         int userNo = (int) session.getAttribute("no");
+
+        User user = userService.getUserByNo(userNo).get();
         Post post = postService.getPost(postNo).get();
+
         ModelAndView mv = new ModelAndView();
-        if (userNo != post.getUserNo()) {
+        if (userNo == post.getUserNo() || user.getUserTypeCode() == 1) {
+            mv.addObject("post", post);
+            mv.setViewName("post/postUpdateForm");
+        } else {
             mv.setViewName("redirect:/post/postView?no=" + postNo);
-        }else{
-        mv.addObject("post",post);
-        mv.setViewName("post/postUpdateForm");
         }
         return mv;
     }
@@ -41,19 +48,23 @@ public class PostUpdateController {
     public ModelAndView doUpdatePost(@RequestParam("no") int postNo,
                                      @RequestParam("postTitle") String postTitle,
                                      @RequestParam("postContent") String postContent,
-                                     HttpServletRequest request){
+                                     HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         int userNo = (int) session.getAttribute("no");
+
+        User user = userService.getUserByNo(userNo).get();
         Post post = postService.getPost(postNo).get();
+
         ModelAndView mv = new ModelAndView();
-        if (userNo != post.getUserNo()) {
-            mv.setViewName("redirect:/post/postView?no=" + postNo);
-        }else{
+
+        if (userNo == post.getUserNo() || user.getUserTypeCode() == 1) {
             post.setPostTitle(postTitle);
             post.setPostContent(postContent);
             post.setModifierUserNo(userNo);
             post.setModifyDatetime(new Timestamp(new Date().getTime()));
             postService.updatePost(post);
+            mv.setViewName("redirect:/post/postView?no=" + postNo);
+        } else {
             mv.setViewName("redirect:/post/postView?no=" + postNo);
         }
         return mv;
